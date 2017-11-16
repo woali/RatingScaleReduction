a7603a5571e55ca5b26642ee5675f96edfaa8c95
Alicja Wolny-Dominiak, phd <woalicja@gmail.com>
2017-11-16 10:52:20 +0100

    Add files via upload

--- example_Rj.r
+++ example_Rj.r
@@ -1,4 +1,4 @@
-#####code by Alicja Wolny-Dominiak
+#####code by Alicja Wolny-Dominiak, Feng Li
 #####woali@ue.katowice.pl
 
 library(RatingScaleReduction)
@@ -7,36 +7,37 @@ library(RatingScaleReduction)
 ###########################################
 #First Example 
 
-mydata=read.csv(file="http://web.ue.katowice.pl/woali/BDI.csv")
+mydata = read.csv(file="http://web.ue.katowice.pl/woali/BDI.csv")
 attach(mydata)
 
-attribute=mydata[,1:21]
-D=mydata[,22]
-rauc.bdi <-totalAuc(attribute, D, plotT=TRUE)
+attribute = mydata[,1:21]
+D = mydata[,22]
+tauc.bdi <- totalAuc(attribute, D, plotT = TRUE)
 
-totalAuc(attribute, D)
-diffExamples(attribute)
-startAuc(attribute, D)
+tauc.bdi$summary
+tauc.bdi$item
 
-rsr.bdi <-rsr(attribute, D, plotRSR=TRUE)
+rsr.bdi <- rsr(attribute, D, plotRSR = TRUE)
+rsr.bdi
 
-###Graded Response Model
+##Graded Response Model (GRM)
 library(ltm)
-grm1 <-grm(attribute)
-grm1.rsr <-grm(data.frame(BDI_1,BDI_7,BDI_9,BDI_10,BDI_14,BDI_15))
+grm1 <- grm(attribute)
+grm1.rsr <- grm(data.frame(BDI_1,BDI_7,BDI_9,BDI_10,BDI_14,BDI_15))
+
 
 ###CFA
 library(lavaan)
 library(semPlot)
 
-model1<-'D=~BDI_1+BDI_2+BDI_3+BDI_4+BDI_5+BDI_6+BDI_7+BDI_8+BDI_9+BDI_10+BDI_11+BDI_12+BDI_13+BDI_14+BDI_15+BDI_16+BDI_17+BDI_18+BDI_19+BDI_20+BDI_21'
-cfa1<-cfa(model1, data=mydata, ordered=c("BDI_1","BDI_2", "BDI_3","BDI_4","BDI_5","BDI_6","BDI_7","BDI_8",
+model1 <- 'D=~BDI_1+BDI_2+BDI_3+BDI_4+BDI_5+BDI_6+BDI_7+BDI_8+BDI_9+BDI_10+BDI_11+BDI_12+BDI_13+BDI_14+BDI_15+BDI_16+BDI_17+BDI_18+BDI_19+BDI_20+BDI_21'
+cfa1 <- cfa(model1, data=mydata, ordered=c("BDI_1","BDI_2", "BDI_3","BDI_4","BDI_5","BDI_6","BDI_7","BDI_8",
 "BDI_9","BDI_10","BDI_11","BDI_12","BDI_13","BDI_14","BDI_15","BDI_16","BDI_17","BDI_18",
 "BDI_19","BDI_20","BDI_21"))
 summary(cfa1, fit.measure=TRUE,standardize=TRUE)
 
-model1.rsr<-'D=~BDI_1+BDI_7+BDI_9+BDI_10+BDI_14+BDI_15'
-cfa1.rsr<-cfa(model1.rsr, data=mydata, ordered=c("BDI_1","BDI_7", "BDI_9","BDI_10","BDI_14","BDI_15"))
+model1.rsr <- 'D=~BDI_1+BDI_7+BDI_9+BDI_10+BDI_14+BDI_15'
+cfa1.rsr <- cfa(model1.rsr, data=mydata, ordered=c("BDI_1","BDI_7", "BDI_9","BDI_10","BDI_14","BDI_15"))
 summary(cfa1.rsr, fit.measure=TRUE,standardize=TRUE)
 
 #layout(t(1:2))
@@ -49,79 +50,47 @@ title("BDI depresion reduced scale", line = 1)
 ##########################################
 ##########################################
 #Second Example 
-mydata=read.csv2(file="http://web.ue.katowice.pl/woali/data312.csv")
+
+##Data manipulation
+mydata_red = read.csv2(file = "http://web.ue.katowice.pl/woali/winequality-red.csv")
+mydata_white = read.csv2(file = "http://web.ue.katowice.pl/woali/winequality-white.csv")
+
+mydata = rbind(mydata_red,mydata_white)
+mydata$decision = 0
+
+for (i in 1:nrow(mydata))
+{
+  if (mydata$quality[i] > 5)
+    mydata$decision[i] = 1
+}
+
+mydata = mydata[,-12]
+
 attach(mydata)
 
 for (i in 1:ncol(mydata)){
-mydata[,i] <-as.numeric(mydata[,i])
+  mydata[,i] <- as.numeric(mydata[,i])
 }
 
-att <-mydata[,2:ncol(mydata)]
-d3 <-match("hepato",names(att))
+##RSR
+attribute <- mydata[,1:11]
+D <- mydata[,12]
+colnames(attribute) <- c("FA","VA","CA","RS","Ch","FSD","TSD","De","pH","Su","Al")
 
-a3 <-totalAuc(att[,-d3], hepato, plotT=TRUE)
-a3$summary
+rauc.wine <- totalAuc(attribute, D, plotT=TRUE)
 
-r3 <-rsr(att[,-d3], hepato, plotRSR=TRUE)
-r3$summary
+rauc.wine$summary
+rauc.wine$item
 
-#Rfig3
-rr <-rowSums(a3$ordered.attribute[,1:2])
+rsr(attribute, D,plotRSR=TRUE)
+diffExamples(attribute)
+startAuc(attribute, D)
 
-par(mfrow=c(1,2))
-plot(a3$total.auc, xaxt="n", type='l', ylab="Total AUC", xlab="Attribute label", lwd=2, main="Total AUC", 
-cex.main=1.3, cex.lab=1.3, cex.axis=1.3)
-points(which.max(a3$total.auc),max(a3$total.auc), col='red', lwd=2,cex=2, cex.main=1.3)
-abline(v=which.max(a3$total.auc), col='blue', lty=2, lwd=2)
-mtext(c(a3$item[1:2],3:18),at=1:length(a3$item),side=1, cex=1.2)
-plot.roc(hepato, rr, plotROC=TRUE, print.auc=TRUE, main=" ROC curve of AUC of the reduced rating scale",
-cex.main=1.3, cex.lab=1.3, cex.axis=1.3)
+rsr.wine <- rsr(attribute, D, plotRSR=TRUE)
 
-#Test deLong, bootsrap
-ch1 <-CheckAttr4Inclusion(att[,-d3], hepato, method=c("delong"), alternative=c("two.side"))
-ch2 <-CheckAttr4Inclusion(att[,-d3], hepato, method=c("bootstrap"), alternative=c("two.side"))
-ch1
-ch2
+rsr.wine$rsr.auc
+rsr.wine$rsr.label
+rsr.wine$summary
 
-#Rfig4
-x=1-ch1$roc1$specificities
-y=ch1$roc1$sensitivities
 
-x1=1-ch1$roc2$specificities
-y1=ch1$roc2$sensitivities
-
-plot(x,y, type='l', lwd=2)
-lines(x1,y1, type='l', lwd=2, col='red')
-legend("bottomright", lwd=c(1,2), col=c('black','red'), c(paste('AUC rating scale reduced (stage, bili)=',round(ch1$roc1$auc,3)), 
-paste("AUC rating scale (stage, bili, albumin)",round(a3$total.auc[3],3))))
-abline(0,1,lwd=2, col='gray')
-
-#Test summary
-summ <-rbind(cbind(ch1$statistic, ch1$p.value), cbind(ch2$statistic, ch2$p.value))
-colnames(summ) <-c("Z statistics", "p-value")
-rownames(summ) <-c("DeLong", "bootstrap")
-summ
-
-##list of gray examples
-sex <-as.numeric(sex)
-
-gray.ex <-c()
-df1 <-unique(data.frame(hepato, status, sex, spiders, stage))
-
-for (i in 1:nrow(df1)){
-ex <-grayExamplesN(df1[,2:ncol(df1)], df1[,1], i)$examp
-if (nrow(ex)>1){
-gray.ex <-rbind(gray.ex, ex[1,])}
-}
-colnames(gray.ex) <-names(df1)
-gray.ex
-
-#all pairs of gray examples
-grayExamples(att, D)
-
-detach(data.ins)
-
-###Graded Response Model
-grm2 <-grm(att[,-d3])
-grm22 <-grm(att[,-d3], constrained=TRUE)
 
