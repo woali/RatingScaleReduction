04520c7eb1085eac16b50176a0f8a9ecb138be2a
Alicja Wolny-Dominiak, phd <woalicja@gmail.com>
2017-11-16 11:06:00 +0100

    Delete diffExamples.R

--- R/diffExamples.R
+++
@@ -1,7 +0,0 @@
-diffExamples <-
-function(attribute){
-m1 <-unique(attribute)
-u <-nrow(m1)
-d <-nrow(as.matrix(attribute))-u
-return(list(total.examples=nrow(as.matrix(attribute)), dif.examples=u, dup.examples=d))
-}
